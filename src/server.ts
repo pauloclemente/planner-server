73db24dba20121fbf0f9f6d16107c3f5523bdc93
Paulo Clemente <clementedbarros@gmail.com>
2024-09-28 20:28:10 -0300

    feat: create confirm trip route

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,18 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import cors from '@fastify/cors'
+import { confirmTrip } from './routes/confirm-trip'
 
 const app = fastify()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(cors)
+app.register(cors, {
+  origin: true,
+})
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('listening on port 3333')
