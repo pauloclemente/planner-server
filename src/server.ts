c979b824f7c3c969b5f4ec3f324145e5946359c9
Paulo Clemente <clementedbarros@gmail.com>
2024-09-29 11:17:37 -0300

    feat: create links create and get routes

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { confirmTrip } from './routes/confirm-trip'
 import { confirmParticipant } from './routes/confirm-participants'
 import { createActivity } from './routes/create-activity'
 import { getActivities } from './routes/get-activities'
+import { createLink } from './routes/create-links'
+import { getLinks } from './routes/get-links'
 
 const app = fastify()
 
@@ -23,6 +25,8 @@ app.register(confirmTrip)
 app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('listening on port 3333')
